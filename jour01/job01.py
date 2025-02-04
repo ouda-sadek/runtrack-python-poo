f5e8ca229df8fcfc0cdecd9b072c0de3d8f4ea06
ouda-sadek <ouda.sadek@laplateforme.io>
2025-02-04 10:38:32 +0100

    commit before merge

--- jour01/job01.py
+++ jour01/job01.py
@@ -1,5 +1,6 @@
+# Creating class Operation()
 class Operation:
-    # The constructor (__init__) initializes the 'number1' and 'number2' attributes with default values
+    # The constructor (__init__) initializes the "number1" and "number2" attributes with default values
     def __init__(self, nombre1=0, nombre2=0):
         self.nombre1 = nombre1
         self.nombre2 = nombre2

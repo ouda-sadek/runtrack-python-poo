9235ae8f2328fa00cb0f1b6b61f9bd71ff041592
ouda-sadek <ouda.sadek@laplateforme.io>
2025-02-05 03:45:34 +0100

    job09, replace print() in afficher method by return & print outside the class

--- jour01/job09.py
+++ jour01/job09.py
@@ -1,62 +1,62 @@
-# Create class 
 class Produit:
     # Initialize the product attributes
     def __init__(self, nom, prixHT, TVA):
-        self.nom = nom
-        self.prixHT = prixHT
-        self.TVA = TVA
+        self.__nom = nom
+        self.__prixHT = prixHT
+        self.__TVA = TVA
         
     # Method to apply TVA
     def calculerPrixTTC(self):
-        return self.prixHT * (1 + self.TVA/100)
+        return self.__prixHT * (1 + self.__TVA / 100)
 
-    # Method to display the product information
+    # Method to return the product information as a string
     def afficher(self):
-        print(f"Produit: {self.nom}")
-        print(f"Prix HT: {self.prixHT}€")
-        print(f"TVA: {self.TVA}%")
-        print(f"Prix TTC: {self.calculerPrixTTC()}€\n")
-
+        return (
+            f"Produit: {self.__nom}\n"
+            f"Prix HT: {self.__prixHT}€\n"
+            f"TVA: {self.__TVA}%\n"
+            f"Prix TTC: {self.calculerPrixTTC()}€\n"
+        )
     
     # Method to calculate the product's price after applying TVA
     def prixTTC(self):
-        return self.prixHT * (1 + self.TVA/100)
+        return self.__prixHT * (1 + self.__TVA / 100)
     
     # Method to change the product name
     def modifierNom(self, nouveau_nom):
-        self.nom = nouveau_nom
+        self.__nom = nouveau_nom
 
     # Method to change the product price
     def modifierPrix(self, nouveau_prix):
-        self.prixHT = nouveau_prix
+        self.__prixHT = nouveau_prix
 
-    
     # Method to calculate the discounted price after applying a given discount
     def prixAvecRemise(self, pourcentageRemise):
-        return self.prixTTC() * (1 - pourcentageRemise/100)
+        return self.prixTTC() * (1 - pourcentageRemise / 100)
     
     # Methods for returning product information
     def getNom(self):
-        return self.nom
+        return self.__nom
     
     def getPrixHT(self):
-        return self.prixHT
+        return self.__prixHT
     
     def getTVA(self):
-        return self.TVA
+        return self.__TVA
     
     def getPrixTTC(self):
         return self.calculerPrixTTC()
-    
+
+
 # Creation of several products
 produit1 = Produit("Ordinateur", 3500, 20)
 produit2 = Produit("Smartphone", 1200, 20)
 produit3 = Produit("Tablette", 950, 20)
 
-# Displaying initial information
-produit1.afficher()
-produit2.afficher()
-produit3.afficher()
+# Displaying initial information by calling the 'afficher' method and using print()
+info_produit1 = produit1.afficher()
+info_produit2 = produit2.afficher()
+info_produit3 = produit3.afficher()
 
 # Editing product information
 produit1.modifierNom("Ordinateur portable")
@@ -68,49 +68,17 @@ produit2.modifierPrix(900)
 produit3.modifierNom("Tablette Pro")
 produit3.modifierPrix(650)
 
-# Displaying information after modification
-produit1.afficher()
-produit2.afficher()
-produit3.afficher()
+# Displaying information after modification by calling the 'afficher' method and using print()
+nouveau_info_produit1 = produit1.afficher()
+nouveau_info_produit2 = produit2.afficher()
+nouveau_info_produit3 = produit3.afficher()
 
 # Results
-"""Produit: Ordinateur
-Prix HT: 3500€
-TVA: 20%
-Prix TTC: 4200.0€
+# To display these results, you will use print() outside the class:
+print(info_produit1)
+print(info_produit2)
+print(info_produit3)
 
-Produit: Smartphone
-Prix HT: 1200€
-TVA: 20%
-Prix TTC: 1440.0€
-
-Produit: Tablette
-Prix HT: 950€
-TVA: 20%
-Prix TTC: 1140.0€
-
-Produit: Ordinateur portable
-Prix HT: 1200€
-TVA: 20%
-Prix TTC: 1440.0€
-
-Produit: Smartphone 5G
-Prix HT: 900€
-TVA: 20%
-Prix TTC: 1080.0€
-
-Produit: Tablette Pro
-Prix HT: 650€
-TVA: 20%
-Prix TTC: 780.0€"""
-
-    
-
-
-
-    
-
-   
-
-   
-
+print(nouveau_info_produit1)
+print(nouveau_info_produit2)
+print(nouveau_info_produit3)
